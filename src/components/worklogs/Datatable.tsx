f4c829ec4098180ac54d4480510a059539a5240a
HarshTechnomark <harsh.dhaduk@technomark.io>
2023-10-12 12:07:21 +0530

    updated code

--- src/components/worklogs/Datatable.tsx
+++ src/components/worklogs/Datatable.tsx
@@ -1252,8 +1252,19 @@ const Datatable = ({
             <div className="flex items-center gap-2">
               {value}
               {IsRecurring && (
-                <span className="text-[20px] text-secondary font-semibold">
-                  &reg;
+                <span className="text-secondary font-semibold">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 24 24"
+                    height="20px"
+                    width="20px"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M12.14 2a10 10 0 1 0 10 10 10 10 0 0 0-10-10zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z"></path>
+                    <path d="M16.14 10a3 3 0 0 0-3-3h-5v10h2v-4h1.46l2.67 4h2.4l-2.75-4.12A3 3 0 0 0 16.14 10zm-3 1h-3V9h3a1 1 0 0 1 0 2z"></path>
+                  </svg>
                 </span>
               )}
             </div>
