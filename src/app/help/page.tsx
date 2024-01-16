9999ff7ff93482d8837a5d003b7fbbc33a7f1356
HarshTechnomark <harsh.dhaduk@technomark.io>
2024-01-16 12:38:05 +0530

    updated help page

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -17,7 +17,9 @@ const Page = () => {
           </div>
         </div>
         <div className="mx-4 px-5 flex justify-between items-center w-[97%] h-28 bg-whiteSmoke rounded-lg shadow-xl">
-          <span>PMS EXE (Version 1.0) December 11, 2023</span>
+          <span>
+            PMS Executable (.exe) File [Version 1.0] - December 11, 2023
+          </span>
           <Button variant="contained" className="!bg-secondary">
             <a href="https://pmsaea8.blob.core.windows.net/prod-help-video-setup/PGCTimeTracker.msi">
               Download
@@ -36,8 +38,8 @@ const Page = () => {
             </li>
             <li className="pb-1">
               Ensure that the user system has the most recent version of the PMS
-              Executable (.exe) File i.e. PMS EXE [Version 1.0] - December 11,
-              2023 and remove any outdated version if any.
+              Executable (.exe) File [Version 1.0] - December 11, 2023 and
+              remove any outdated version if any.
             </li>
             <li className="pb-1">
               In the event of a password change on the PMS web portal, it is
