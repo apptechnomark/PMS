926151e49ce44dc51ceb6f2e4b0425502824ce08
HarshTechnomark <harsh.dhaduk@technomark.io>
2024-03-06 11:09:27 +0530

    updated code with report chagnes

--- src/components/reports/tables/BillingReport.tsx
+++ src/components/reports/tables/BillingReport.tsx
@@ -565,6 +565,17 @@ const BillingReport = ({
         onPageChange={handleChangePage}
         rowsPerPage={billingRowsPerPage}
         onRowsPerPageChange={handleChangeRowsPerPage}
+        rowsPerPageOptions={
+          billingReportFields.dataCount <= 10
+            ? [10, 25, 50, 100]
+            : billingReportFields.dataCount <= 20
+            ? [10, billingReportFields.dataCount]
+            : billingReportFields.dataCount <= 30
+            ? [10, 20, billingReportFields.dataCount]
+            : billingReportFields.dataCount <= 50
+            ? [10, 25, billingReportFields.dataCount]
+            : [10, 25, 50, billingReportFields.dataCount]
+        }
       />
     </ThemeProvider>
   ) : (
